736608097037b09f8ad404b7ca94c1a0398dc369
sarveshash <136432405+sarveshash@users.noreply.github.com>
2024-03-28 14:13:22 +0530

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -1,33 +1,27 @@
-# Import all necessary modules
- 
 from Bot_Details import *
 from pyrogram import Client, filters
-import asyncio
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardMarkup,CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
-
-#Initiate the Client
-
+# Initiate the Client
 app = Client("xyz", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 text = "hi"
 text_button = [
     [
-        InlineButtonKeyboard("next",callback_data= "go_next")
+        InlineKeyboardButton("next", callback_data="go_next")
     ]
 ]
 
-@app.on_message(filters.command('launch')&filters.private)
-async def launch(bot,message) :
+@app.on_message(filters.command('launch') & filters.private)
+async def launch(bot, message):
     await message.reply(
-        text = text,
-        reply_markup = text_button
+        text=text,
+        reply_markup=InlineKeyboardMarkup(text_button)
     )
 
 @app.on_callback_query()
-def callback_query(client, callbackQuery):
-    if CallbackQuery.data == "go_next":
-        await CallbackQuery.edit.message_video(message.chat.id,"BAACAgUAAxkBAAPWZgKNra82bPHl-IL9-4HPcg8uGrkAAqERAAKSdRhUrVsfGHzt9YIeBA")
+async def callback_query(client, callbackQuery):
+    if callbackQuery.data == "go_next":
+        await callbackQuery.message.edit_media(media="BAACAgUAAxkBAAPWZgKNra82bPHl-IL9-4HPcg8uGrkAAqERAAKSdRhUrVsfGHzt9YIeBA")
 
 app.run()
-
